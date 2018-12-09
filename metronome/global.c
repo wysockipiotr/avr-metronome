f8fe3948fd86f80108928e39a690fb20ad3894a6
96wysocki <96wysocki@gmail.com>
2018-12-09 17:13:52 +0100

    Global variables moved from main.c to global.c

--- metronome/global.c
+++ metronome/global.c
@@ -1,62 +1,54 @@
-#ifndef __global_h__
-#define __global_h__
-
-#include "util.h"
-#include "lcd.h"
-
 #include <avr/io.h>
-#include "global.h"
 #include "util.h"
+#include "global.h"
 
 // currently controlled parameter (highlighted on screen)
 // [0] bpm [1, 2] signature [3] volume [4] mode
-extern volatile uint8_t cursor;
+volatile uint8_t cursor;
 
 // '>' for switching parameters, '}' for adjusting value
-extern volatile char cursor_symbol;
+volatile char cursor_symbol;
 
 // if true, adjusting value of highlighted parameter
 // if false, controlled parameter switching
-extern volatile bool edit_active;
+volatile bool edit_active;
 
 // beats per minute
-extern volatile uint16_t bpm;
+volatile uint16_t bpm;
 
 // index of time signature chosen from signatures array
-extern volatile uint8_t signature;
+volatile uint8_t signature;
 
 // duration of metronome beep
-extern volatile uint16_t t_beep;
+volatile uint16_t t_beep;
 
 // duration of silence between beeps
-extern volatile uint16_t t_sleep;
+volatile uint16_t t_sleep;
 
 // sound volume expressed in percent
-extern volatile uint8_t volume;
+volatile uint8_t volume;
 
 // counter measuring interval between last two taps in miliseconds
-extern volatile uint16_t tap_interval_counter;
+volatile uint16_t tap_interval_counter;
 
 // flag indicating whether tap tempo mode is currently active
-extern volatile bool tap_started;
+volatile bool tap_started;
 
 // flag blocking output to speaker
-extern volatile bool sound_locked;
+volatile bool sound_locked;
 
 // current metronome mode (0 = SOUND_MODE, 1=VIBRT_MODE)
-extern volatile bool mode;
+volatile bool mode;
 
 // lcd backlight timer (in miliseconds)
-extern volatile uint16_t backlight_time_counter;
+volatile uint16_t backlight_time_counter;
 
 // indicates whether lcd backlight is on
-extern volatile bool backlight_on;
+volatile bool backlight_on;
 
 // available time signatures
-extern uint8_t signatures[4];
+uint8_t signatures[4] = {0x44, 0x34, 0x54, 0x74};
 
 // content to be displayed on LCD
-extern char firstLineBuffer[LCD_SIZE + 1];
-extern char secondLineBuffer[LCD_SIZE + 1];
-
-#endif
+char firstLineBuffer[LCD_SIZE + 1];
+char secondLineBuffer[LCD_SIZE + 1];
--- metronome/main.c
+++ metronome/main.c
@@ -3,58 +3,7 @@
 #include "lcd.h"
 #include "metronome.h"
 #include "util.h"
-
-// currently controlled parameter (highlighted on screen)
-// [0] bpm [1, 2] signature [3] volume [4] mode
-volatile uint8_t cursor;
-
-// '>' for switching parameters, '}' for adjusting value
-volatile char cursor_symbol;
-
-// if true, adjusting value of highlighted parameter
-// if false, controlled parameter switching
-volatile bool edit_active;
-
-// beats per minute
-volatile uint16_t bpm;
-
-// index of time signature chosen from signatures array
-volatile uint8_t signature;
-
-// duration of metronome beep
-volatile uint16_t t_beep;
-
-// duration of silence between beeps
-volatile uint16_t t_sleep;
-
-// sound volume expressed in percent
-volatile uint8_t volume;
-
-// counter measuring interval between last two taps in miliseconds
-volatile uint16_t tap_interval_counter;
-
-// flag indicating whether tap tempo mode is currently active
-volatile bool tap_started;
-
-// flag blocking output to speaker
-volatile bool sound_locked;
-
-// current metronome mode (0 = SOUND_MODE, 1=VIBRT_MODE)
-volatile bool mode;
-
-// lcd backlight timer (in miliseconds)
-volatile uint16_t backlight_time_counter;
-
-// indicates whether lcd backlight is on
-volatile bool backlight_on;
-
-// available time signatures
-uint8_t signatures[4] = {0x44, 0x34, 0x54, 0x74};
-
-// content to be displayed on LCD
-char firstLineBuffer[LCD_SIZE + 1];
-
-char secondLineBuffer[LCD_SIZE + 1];
+#include "global.h"
 
 ISR(PCINT2_vect) {
     handle_portd_pin_change();
